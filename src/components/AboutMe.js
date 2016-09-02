83f6dc9dd74c0abb725a9107a20ea81e3b12e688
TJ Alston <tjalston82@yahoo.com>
2016-09-02 02:17:51 -0400

    about me added

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,14 +4,22 @@ class AboutMe extends Component {
 
   render () {
     return <div>
-      <h2>About Me</h2>
+      <h2>Somee Tidbits About Me</h2>
       <div className='about'>
       <div className='newhaven' />
-      <p className='homeA'> Born an raised in New Haven Connecticut, home to Yale University and known as the "Cultural Capital of Connecticut". I was always exposed to the arts when I was growing up. During my childhood I was always either in photography, video, dance, drawing, painting, choir or theatre </p>
+      <p className='homeA'> Born an raised in New Haven Connecticut, home to Yale University and known as the "Cultural Capital of Connecticut". I was always exposed to the arts when I was growing up. During my childhood I was always either in photography, video, dance, drawing, painting, choir or theatre. </p>
       </div>
       <div className='about'>
       <p className='homeP'> In June of 2000, I graduated from Cooperative Arts and Humanities High School at that time it was the only full-time arts High School in Connecticut.  I graduated as a Visual Arts major with a concentration in Video and Photography.  During that time I was Secretary of the Senior Class, Editor in Cheif of our class Yearbook and Tresurer of the Recycling Club.  I was also inducted into the National Art Honor Society that same year. </p>
       <div className='coop' />
+      </div>
+      <div className='about'>
+      <div className='uncw' />
+      <p className='homeA'> I attended college at the University of North Carolina and Wilmington. In 2003, I joined the Pi Nu Chapter of Zeta Phi Beta Sorority Incorporated. I graduated in 2004 with a Bachelor of Arts Degree in Communication Studies and a minor in Theater.   Worked on the student television channel Studio 124 as a Technical Director and Set Designer.  I also graduated with the honor of being on the Dean's List my last two consecutive years. </p>
+      </div>
+      <div className='about'>
+      <p className='homeP'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/sW8PQT-uhbw" frameBorder="0" allowFullScreen></iframe>
       </div>
     </div>
   }
